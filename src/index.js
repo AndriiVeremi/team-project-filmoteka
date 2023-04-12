701ea33e05a0d79a2def6cde89defda82792dcb5
Andrii Veremii <dashuk10@gmail.com>
2023-04-12 23:06:02 +0300

    add Notify

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import getRefs from './js/refs';
 import APIservice from './js/fetch-API';
 
 // const refs = getRefs();
-
+// const API = new APIservice();
--- src/js/fetch-API.js
+++ src/js/fetch-API.js
@@ -1,4 +1,56 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const BASE_URL = '';
-const API_KEY = '';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '5257856f789bada50296aabdc3a8b8f3';
+
+class ApiClient {
+
+    constructor() {
+        this.page = 1;
+        this.searchQuery = '';      
+    }
+
+    async getMoveTrending() {
+        try {
+            const fetchLink = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=en-US&page=${this.page}`;
+            const response = await axios.get(fetchLink);
+            return response;
+        } catch (error) {
+            Notify.failure(`Sorry. Please try again.`);
+        }
+    }
+
+    async getMoveName() {
+        try {
+            const fetchLink = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+            const response = await axios.get(fetchLink);
+            return response;
+        } catch (error) {
+            Notify.failure(`Sorry. Please try again.`);
+        }
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    decrementPage() {
+        this.page -= 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+}
+
+export default ApiClient;
