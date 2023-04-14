f8bfb6c99a37b592d0c51956863bcbca8aec30af
Andrii Veremii <dashuk10@gmail.com>
2023-04-14 20:27:02 +0300

    add

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ popularMovies();
 
 
 
-import './js/fetch-API.js';
+// import './js/fetch-API.js';
 // import './js/localStorage.js';
 // import './js/openModal.js';
 // import './js/pagination.js';
-import './js/popularMovies';
-import './js/renderMarkup';
-import './js/searchFilms';
+// import './js/popularMovies';
+// import './js/renderMarkup';
+// import './js/searchFilms';
 // import './js/spinner';
