38b8b43784d45295a75a6503de827d5a0b478a7e
Andrii Veremii <dashuk10@gmail.com>
2023-04-14 21:40:14 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 
 
-// import getRefs from './js/refs';
-// import APIservice from './js/fetch-API';
-import { popularMovies } from './js/popularMovies';
-
-// const refs = getRefs();
-// const API = new APIservice(); 
-
-// При завантажені сторінки відразу загружаються популярні фільми
-popularMovies();
 
 
 
@@ -16,11 +7,12 @@ popularMovies();
 
 
 
-// import './js/fetch-API.js';
+
+import './js/fetch-API.js';
 // import './js/localStorage.js';
 // import './js/openModal.js';
 // import './js/pagination.js';
-// import './js/popularMovies';
-// import './js/renderMarkup';
-// import './js/searchFilms';
+import './js/popularMovies';
+import './js/renderMarkup';
+import './js/searchFilms';
 // import './js/spinner';
