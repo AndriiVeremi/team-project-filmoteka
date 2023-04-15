1e359766597e72cfb03bcc39c0632502e5a682d8
Andrii Veremii <dashuk10@gmail.com>
2023-04-15 15:16:21 +0300

    add

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,16 +2,16 @@ export default function getRefs() {
     const refs = {
         gallery: document.querySelector('.gallery'),
         goTopBtn: document.querySelector('.go-top'),
-        
+
         queueBtn: document.querySelector('.queueBtn'),
         addToQueueBtn: document.querySelector('.addToQueueBtn'),
         watchedBtn: document.querySelector('.watchedBtn'),
         addToWatchedBtn: document.querySelector('.addToWatchedBtn'),
         
-        modalWindow: document.querySelector('.'),
+        // modalWindow: document.querySelector(''),
         overlay: document.querySelector('.overlay'),
-        btnCloseModal: document.querySelector('.'),
-        btnOpenModal: document.querySelectorAll('.'),
+        // btnCloseModal: document.querySelector(''),
+        // btnOpenModal: document.querySelectorAll(''),
 
 
     };
--- src/js/renderTeam.js
+++ src/js/renderTeam.js
@@ -1,30 +1,30 @@
-import { dataTeam } from './dataTeam.js';
-// import  foto  from 'url:../../src/images/team/andrii.png'
-// console.log(foto)
-import getRefs from './refs.js';
+// import { dataTeam } from './dataTeam.js';
+// // import  foto  from 'url:../../src/images/team/andrii.png'
+// // console.log(foto)
+// import getRefs from './refs.js';
 
-const refs = getRefs();
+// const refs = getRefs();
 
-export function renderTeam(dataTeam) {   
-    const team = dataTeam.map(({ imgUrl, name, role, gitUrl, gitImg, }) => {
-        return `      
-        <div class="team-card">
-           <img src="${imgUrl}" alt="Team foto" class="team-image">
-              <p class="team-name">${name}</p>
-              <p class="team-role">${role}</p>
+// export function renderTeam(dataTeam) {   
+//     const team = dataTeam.map(({ imgUrl, name, role, gitUrl, gitImg, }) => {
+//         return `      
+//         <div class="team-card">
+//            <img src="${imgUrl}" alt="Team foto" class="team-image">
+//               <p class="team-name">${name}</p>
+//               <p class="team-role">${role}</p>
 
-            <a href="${gitUrl}" target="_blank" class="team-git">
-             <svg class="ico" width="24" height="24">
-              <use href="${gitImg}"></use>
-             </svg>
-            </a>
-        </div>`;
-    }).join("");
+//             <a href="${gitUrl}" target="_blank" class="team-git">
+//              <svg class="ico" width="24" height="24">
+//               <use href="${gitImg}"></use>
+//              </svg>
+//             </a>
+//         </div>`;
+//     }).join("");
 
-    refs.gallery.insertAdjacentHTML("beforeend", team);  
-}
+//     refs.gallery.insertAdjacentHTML("beforeend", team);  
+// }
 
-console.log(renderTeam(dataTeam))
+// console.log(renderTeam(dataTeam))
 
 
 
