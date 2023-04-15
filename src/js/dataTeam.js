782b4b50a8690d907f5bdf181290ac8b9866ccfa
Andrii Veremii <dashuk10@gmail.com>
2023-04-15 16:10:58 +0300

    add

--- src/js/dataTeam.js
+++ src/js/dataTeam.js
@@ -1,27 +1,35 @@
+import photoAndrii from "../images/team/andrii.png";
+import photoAlla from "../images/team/alla.png";
+import photoIvan from "../images/team/ivan.png";
+import photoIryna from "../images/team/iryna.png";
+
+
+
+
 export const dataTeam = [
     {
-        imgUrl: 'url../../src/images/team/andrii.png',
+        imgUrl: photoAndrii,
         name: 'Andrii Veremii',
-        role: 'Teamlid',
+        role: 'Team Lead',
         gitUrl: 'url',
         gitImg: 'url',
     },
     {
-        imgUrl: '/src/images/team/alla.png',
+        imgUrl: photoAlla,
         name: 'Alla Zubko',
         role: 'ScramMaster',
         gitUrl: 'url',
         gitImg: 'url',
     },
     {
-        imgUrl: '#',
+        imgUrl: photoIvan,
         name: 'Ivan Skidan',
         role: 'Developer',
         gitUrl: 'url',
         gitImg: 'url',
     },
     {
-        imgUrl: '#',
+        imgUrl: photoIryna,
         name: 'Iryna Kyslytsia',
         role: 'Developer',
         gitUrl: 'url',
@@ -33,6 +41,113 @@ export const dataTeam = [
 
 
 
+// import photoBeresten from "../images/team/Beresten.jpeg";
+// import photoMyhulia from "../images/team/Myhulia.jpeg";
+// // import photo from "../images/team/";  Inna
+// // import photo from "../images/team/";  Oleksandr Mix
+// // import photo from "../images/team/";  Oleksandr H
+// import photoBondarenkoI from "../images/team/BondarenkoI.jpeg";
+// import photoKhymenko from "../images/team/Khymenko.jpg";
+// // import photo from "../images/team/"; Yuliya
+// import photoBondarenkoY from "../images/team/BondarenkoY.jpeg";
+// // import photo from "../images/team/"; Dlada
+// // import photo from "../images/team/"; Yana x
+// // import photo from "../images/team/";  Muxailo
+// // import photo from "../images/team/";  Svetlana
+// // import photo from "../images/team/";  Bogdan
+
+
+
+// export const teamList = [
+//     {
+//         name: 'Dmytrii Beresten',
+//         role: 'Team Lead',
+//         github: 'https://github.com/dmitryberesten',
+//         photo: photoBeresten,
+//     },
+//     {
+//         name: 'Maksym Myhulia',
+//         role: 'Scrum Master',
+//         github: 'https://github.com/MaksymMyhulia',
+//         photo: photoMyhulia,
+//     },
+//     {
+//         name: 'Inna',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Oleksandr',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Oleksandr H',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Iryna Bondarenko',
+//         role: 'Developer',
+//         github: 'https://github.com/IrynaBondarenko7 ',
+//         photo: photoBondarenkoI,
+//     },
+//     {
+//         name: 'Oleksandr Khymenko',
+//         role: 'Developer',
+//         github: 'https://github.com/Crash-afftar',
+//         photo: photoKhymenko,
+//     },
+//     {
+//         name: 'Yuliya',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Yuliya Bondarenko',
+//         role: 'Developer',
+//         github: 'https://github.com/YuliyaBondG',
+//         photo: photoBondarenkoY,
+//     },
+//     {
+//         name: 'Dlada',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Yana',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Myxailo',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+//     {
+//         name: 'Svitlana Chaban',
+//         role: 'Developer',
+//         github: 'https://github.com/Universe2022',
+//         photo: '',
+//     },
+//     {
+//         name: 'Bogdan',
+//         role: 'Developer',
+//         github: '',
+//         photo: '',
+//     },
+
+
+
+
+// ]
 
 
 
