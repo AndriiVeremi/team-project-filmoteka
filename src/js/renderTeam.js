47fb3cdab155fdc5185eefeebb12fe354dd09eb3
Andrii Veremii <dashuk10@gmail.com>
2023-04-15 23:33:49 +0300

    add

--- src/js/renderTeam.js
+++ src/js/renderTeam.js
@@ -1,5 +1,5 @@
-// import * as basicLightbox from 'basiclightbox';
-// import 'basiclightbox/dist/basicLightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 import getRefs from './refs.js';
 
