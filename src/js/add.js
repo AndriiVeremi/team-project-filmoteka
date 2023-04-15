386dcc94b993ac118904de3fa45cf9504a3110b9
Andrii Veremii <dashuk10@gmail.com>
2023-04-15 21:19:20 +0300

    add

--- src/js/add.js
+++ src/js/add.js
@@ -1,5 +1,7 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
+import * as popular from './popularMovies.js';
+
 
 import getRefs from './refs.js';
 import APIservice from './fetch-API.js';
@@ -12,17 +14,22 @@ refs.gallery.addEventListener('click', clickOnMovie);
 async function clickOnMovie(event) {
     event.preventDefault();
 
-    try {
-        let moveId = event.target.dataset.id;
-      
-        console.log(moveId)
-        
-        
-        const response = await API.getMoveInfo(moveId);
 
-        
-    } catch (error){
+
+    try {
+
+        let moveId = event.target.dataset.id;
+        // let id = data.data.results.id
+
+        console.log(moveId)
+
+
+        const response = await API.getMoveInfo(948713);
+        console.log(response)
+
+    } catch (error) {
         console.log('message')
     }
-    
+
 }
+
