37f18caaa8253d60c3b8d8e3bd1d9b889a2ddfb0
Andrii Veremii <dashuk10@gmail.com>
2023-04-16 16:03:11 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import './js/modal.js';
 import './js/refs.js';
 import './js/spinner';
 import './js/fetch-API.js';
 import './js/searchFilms';
 import './js/popularMovies';
 import './js/renderMarkup';
-
 import './js/pagination.js';
 import './js/buttonUp.js';
 import './js/renderTeam.js';
-// import './js/renderModalFilms.js';
 import './js/modalFilms.js';
-
 import './js/localStorage.js';
-// import './js/openModal.js';
+
 
 
 
