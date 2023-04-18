f42fb106cbe1b1b0c46cc55816f7d69f4d35a903
Kirill <stevemcc.man@gmail.com>
2023-04-18 13:52:52 +0300

    .

--- src/js/library.js
+++ src/js/library.js
@@ -17,7 +17,55 @@ refs.queueBtn.style.display = 'none'
 refs.watchedBtn.style.display = 'none'
 
 
-libraryBtnRef.addEventListener('click', openLib);
+// libraryBtnRef.addEventListener('click', openLib);
+async function openLib() {
+  refs.gallery.innerHTML = ''
+  refs.queueBtn.style.display = 'inline-flex'
+  refs.watchedBtn.style.display = 'inline-flex'
+  searchFormRef.style.display = 'none'
+try{
+  if (localStorage.getItem(WATCHED_KEY) === null&&localStorage.getItem(QUEUE_KEY) === null) {
+      refs.gallery.innerHTML = ''
+  }else{
+      JSON.parse(localStorage.getItem(WATCHED_KEY))
+      JSON.parse(localStorage.getItem(QUEUE_KEY))
+  }
+}catch (error) {
+  console.error(message);
+}
+const WatcheMoviesId = JSON.parse(localStorage.getItem(WATCHED_KEY));
+const QueueMoviesId = JSON.parse(localStorage.getItem(QUEUE_KEY));
+const moviesId = WatcheMoviesId.concat(QueueMoviesId)
+console.log(WatcheMoviesId);
+// console.log(QueueMoviesId);
+// console.log(moviesId);
+try {
+  const response = await API.getMoveTrending();
+  refs.gallery.innerHTML = '';
+  console.log(response.data.results);
+  const results = response.data.results;
+  const arrGenreId = results.map(item => item.genre_ids);
+// console.log(arrGenreId);
+const genreResponse = await API.getMoveGanres();
+  const arrGenre = genreResponse.data.genres;
+
+  // console.log(arrGenre);
+  for (const id of moviesId) {
+    // console.log(id);
+    const moveId = results.filter(item => item.id == id);
+    replaceIdtoGenre(arrGenre, arrGenreId);
+    renderMarkup(moveId);
+  }
+} catch (error) {
+  console.log(error);
+}
+}
+
+
+
+
+
+
 
 const registerBtn = document.querySelector('#register-btn');
 const registerModal = document.querySelector('#register-modal');
@@ -53,52 +101,58 @@ registerForm.addEventListener('submit', (e) => {
   // Закрываем модальное окно
   registerModal.style.display = 'none';
 
+  openLib();
+
 });
 
 
+// перезагрузка страницы.
 
-async function openLib() {
-    refs.gallery.innerHTML = ''
-    refs.queueBtn.style.display = 'inline-flex'
-    refs.watchedBtn.style.display = 'inline-flex'
-    searchFormRef.style.display = 'none'
-try{
-    if (localStorage.getItem(WATCHED_KEY) === null&&localStorage.getItem(QUEUE_KEY) === null) {
-        refs.gallery.innerHTML = ''
-    }else{
-        JSON.parse(localStorage.getItem(WATCHED_KEY))
-        JSON.parse(localStorage.getItem(QUEUE_KEY))
-    }
-}catch (error) {
-    console.error(message);
-  }
-  const WatcheMoviesId = JSON.parse(localStorage.getItem(WATCHED_KEY));
-  const QueueMoviesId = JSON.parse(localStorage.getItem(QUEUE_KEY));
-  const moviesId = WatcheMoviesId.concat(QueueMoviesId)
-console.log(WatcheMoviesId);
-  // console.log(QueueMoviesId);
-  // console.log(moviesId);
-  try {
-    const response = await API.getMoveTrending();
-    refs.gallery.innerHTML = '';
-    console.log(response.data.results);
-    const results = response.data.results;
-    const arrGenreId = results.map(item => item.genre_ids);
-// console.log(arrGenreId);
-const genreResponse = await API.getMoveGanres();
-    const arrGenre = genreResponse.data.genres;
 
-    // console.log(arrGenre);
-    for (const id of moviesId) {
-      // console.log(id);
-      const moveId = results.filter(item => item.id == id);
-      replaceIdtoGenre(arrGenre, arrGenreId);
-      renderMarkup(moveId);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+
+
+// async function openLib() {
+//     refs.gallery.innerHTML = ''
+//     refs.queueBtn.style.display = 'inline-flex'
+//     refs.watchedBtn.style.display = 'inline-flex'
+//     searchFormRef.style.display = 'none'
+// try{
+//     if (localStorage.getItem(WATCHED_KEY) === null&&localStorage.getItem(QUEUE_KEY) === null) {
+//         refs.gallery.innerHTML = ''
+//     }else{
+//         JSON.parse(localStorage.getItem(WATCHED_KEY))
+//         JSON.parse(localStorage.getItem(QUEUE_KEY))
+//     }
+// }catch (error) {
+//     console.error(message);
+//   }
+//   const WatcheMoviesId = JSON.parse(localStorage.getItem(WATCHED_KEY));
+//   const QueueMoviesId = JSON.parse(localStorage.getItem(QUEUE_KEY));
+//   const moviesId = WatcheMoviesId.concat(QueueMoviesId)
+// console.log(WatcheMoviesId);
+//   // console.log(QueueMoviesId);
+//   // console.log(moviesId);
+//   try {
+//     const response = await API.getMoveTrending();
+//     refs.gallery.innerHTML = '';
+//     console.log(response.data.results);
+//     const results = response.data.results;
+//     const arrGenreId = results.map(item => item.genre_ids);
+// // console.log(arrGenreId);
+// const genreResponse = await API.getMoveGanres();
+//     const arrGenre = genreResponse.data.genres;
+
+//     // console.log(arrGenre);
+//     for (const id of moviesId) {
+//       // console.log(id);
+//       const moveId = results.filter(item => item.id == id);
+//       replaceIdtoGenre(arrGenre, arrGenreId);
+//       renderMarkup(moveId);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 // перезагрузка страницы.
 homeBtn.addEventListener('click', goToHomePage)
 
