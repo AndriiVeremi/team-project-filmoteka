4124b386437c0b91409df8e70a296d290ca7bf26
Y-Savushchyk <savushchky@gmail.com>
2023-04-18 22:29:14 +0300

    Update library.js

--- src/js/library.js
+++ src/js/library.js
@@ -2,6 +2,8 @@ import getRefs from './refs.js';
 import APIservice from './fetch-API.js';
 import { renderMarkup } from './renderMarkup.js';
 import {replaceIdtoGenre} from './popularMovies.js'
+import { instance } from './pagination.js';
+
 
 
 const API = new APIservice();
@@ -12,53 +14,50 @@ const WATCHED_KEY = 'WatchedFilms';
 const libraryBtnRef= document.querySelector('button[data-page="library"]')
 const homeBtn = document.querySelector('button[data-page="home"]')
 const searchFormRef = document.querySelector('.search-form')
+const containerRef = document.querySelector('.container')
 
 refs.queueBtn.style.display = 'none'
 refs.watchedBtn.style.display = 'none'
 
 
 libraryBtnRef.addEventListener('click', openLib);
-
 async function openLib() {
-    refs.gallery.innerHTML = ''
-    refs.queueBtn.style.display = 'inline-flex'
-    refs.watchedBtn.style.display = 'inline-flex'
-    searchFormRef.style.display = 'none'
-try{
-    if (localStorage.getItem(WATCHED_KEY) === null&&localStorage.getItem(QUEUE_KEY) === null) {
-        refs.gallery.innerHTML = ''
+  refs.gallery.innerHTML = ''
+  refs.queueBtn.style.display = 'inline-flex'
+  refs.watchedBtn.style.display = 'inline-flex'
+  searchFormRef.style.display = 'none'
+  instance.setTotalItems(WATCHED_KEY);
+  instance.reset();
+  try {
+    if (localStorage.getItem(WATCHED_KEY) === null) {
+      refs.gallery.innerHTML = ''
+      // const message = document.createElement('p')
+      // message.textContent = "You haven't added movies yet. Please make your choise first";
+      // refs.gallery.append(message)
+      
     }else{
-        JSON.parse(localStorage.getItem(WATCHED_KEY))
-        JSON.parse(localStorage.getItem(QUEUE_KEY))
+      JSON.parse(localStorage.getItem(WATCHED_KEY))
     }
-}catch (error) {
+  } catch (error) {
     console.error(message);
   }
-  const WatcheMoviesId = JSON.parse(localStorage.getItem(WATCHED_KEY));
-  const QueueMoviesId = JSON.parse(localStorage.getItem(QUEUE_KEY));
-  const moviesId = WatcheMoviesId.concat(QueueMoviesId)
-console.log(WatcheMoviesId);
-  // console.log(QueueMoviesId);
-  // console.log(moviesId);
+  const moviesId = JSON.parse(localStorage.getItem(WATCHED_KEY));
   try {
     const response = await API.getMoveTrending();
     refs.gallery.innerHTML = '';
-    console.log(response.data.results);
     const results = response.data.results;
     const arrGenreId = results.map(item => item.genre_ids);
-// console.log(arrGenreId);
-const genreResponse = await API.getMoveGanres();
+    const genreResponse = await API.getMoveGanres();
     const arrGenre = genreResponse.data.genres;
-
-    // console.log(arrGenre);
     for (const id of moviesId) {
-      // console.log(id);
       const moveId = results.filter(item => item.id == id);
       replaceIdtoGenre(arrGenre, arrGenreId);
+      instance.setTotalItems(WATCHED_KEY);
+      instance.reset();
       renderMarkup(moveId);
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 }
 // перезагрузка страницы.
