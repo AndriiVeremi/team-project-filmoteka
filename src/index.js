3a6e1121b6c3bc99c7c8cfa8c8f40aff8ccc7af0
Andrii Veremii <dashuk10@gmail.com>
2023-04-19 09:16:24 +0300

    add

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './js/renderTeam.js';
 import './js/modalFilms.js';
 import './js/localStorage.js';
 import './js/library.js';
-// import './js/modalFilms.js';
+import './js/changeTheme.js';
 
 
 
