d2723a99f4b67067f670892e3d0de533fd59bd6a
Andrii Veremii <dashuk10@gmail.com>
2023-04-20 13:36:29 +0300

    add
    в renderMarkup додав в тег а   href=" "
    видалив лишнє

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -1,14 +1,12 @@
 import getRefs from './refs.js';
-import APIservice from './fetch-API.js';
 
 const refs = getRefs();
-const API = new APIservice();
 
 export function renderMarkup(results) {
   const markup = results
     .map(({ poster_path, title, id, release_date, genre_ids }) => {
       return `<li class="gallery-item" data-id="${id}">
-    <a href="" class="gallery-link">
+    <a href="${checkedImg(poster_path)}" class="gallery-link">
       <div class="gallery-card">
         <img class="gallery-photo" src="${checkedImg(poster_path)}" alt="${title}" loading="lazy" />
       </div>
@@ -25,8 +23,6 @@ export function renderMarkup(results) {
   refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
  
-
-
 // Перевірка дати виходу фільма
 const dateRelease = value =>
   `${!value ? 'Unknown year' : `${value.slice(0, 4)}`}`;
